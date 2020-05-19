8046b722a206fa19f5c091bbf6e95eaff5eaae7a
Tata Kowshik <tatakowshik@gmail.com>
2020-05-19 12:51:43 +0530

    commit

---
+++ jenkins/src/main/java/com/example/jenkins/App.java
@@ -0,0 +1,16 @@
+package com.example.jenkins;
+
+import com.example.model.Hello;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	
+    public static void main( String[] args )
+    {
+        System.out.println( new Hello().callHi("Tata"));
+    }
+}
---
+++ jenkins/src/main/java/com/example/model/Hello.java
@@ -0,0 +1,9 @@
+package com.example.model;
+
+public class Hello {
+public String callHi(String name)
+{
+	return "Hello"+name;
+	
+}
+}
